59e383ab73bed3719b19f2f04ce1a49d385e5a87
Daniel De La Pava Suarez <daniel.samkit@gmail.com>
2019-12-20 19:30:06 -0500

    added time validation, grade type and private method refactor

--- handler/http/Server.go
+++ handler/http/Server.go
@@ -3,10 +3,10 @@ package http
 import (
 	"../../driver"
 	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
 	"regexp"
+	"sort"
 	"time"
 
 	modelServe "../../model"
@@ -16,7 +16,7 @@ import (
 	domain "../../repository/domain"
 )
 
-func NewServerHandler(db *driver.DB) *Domain{
+func NewServerHandler(db *driver.DB) *Domain {
 	return &Domain{
 		repo: domain.NewSQLDomainRepo(db.SQL),
 	}
@@ -26,12 +26,6 @@ type Domain struct {
 	repo repository.DomainRepo
 }
 
-
-func (rp *Domain) Create(ssl *modelSsl.SSL) {
-
-}
-
-
 func (rp *Domain) GetByAddress(w http.ResponseWriter, r *http.Request) {
 	address := chi.URLParam(r, "address")
 	data, err := GetDataSSl(address)
@@ -39,9 +33,10 @@ func (rp *Domain) GetByAddress(w http.ResponseWriter, r *http.Request) {
 	loc, _ := time.LoadLocation("America/Bogota")
 	var dataServer modelServe.DataServe
 	var servers []modelServe.Serve
+	var detailsDomain []modelDomain.DetailDomain
 	var changeServer bool
 
-	address = ValidateURL(address)
+	address = validateURL(address)
 	payload, err := rp.repo.GetDomainByAddress(r.Context(), address)
 
 	if (modelDomain.Domain{}) == payload {
@@ -56,53 +51,30 @@ func (rp *Domain) GetByAddress(w http.ResponseWriter, r *http.Request) {
 			respondWithError(w, http.StatusNoContent, err.Error())
 		}
 
-		SaveDetailDomain(data, idDomain, rp, w, r)
+		saveDetailDomain(data, idDomain, rp, w, r)
 	} else {
 
-		//detailsDomain, err := rp.repo.GetDetailsByDomain(r.Context(), payload.ID, len(data.Endpoints))
+		detailsDomain, err := rp.repo.GetDetailsByDomain(r.Context(), payload.ID, len(data.Endpoints))
 
 		if err != nil {
 			//log.Fatal(err)
 			respondWithError(w, http.StatusNoContent, err.Error())
 		}
 
-		loc, _ := time.LoadLocation("America/Bogota")
-
-		now := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), 0, loc)
-		//now := time.Now().Round(.000000).In(loc)
-		fmt.Println("\nToday : ", loc, " Time : ", now)
-
-		pastDate := payload.LastConsultation
-		fmt.Println("Time : ", pastDate) //
-		fmt.Printf("###############################################################\n")
-		diff := now.Sub(pastDate)
-
-		hrs := int(diff.Hours()/60.0)
-		fmt.Printf("Diffrence in Hours : %d Hours\n", hrs)
-
-
-		/*hours := time.Now().In(loc).Sub(payload.LastConsultation).Hours()
-		fmt.Println("Hours", hours)
-
-		if hours >= 1 {
-			if len(data.Endpoints) == len(detailsDomain){
-				for i := 0; i < len(data.Endpoints); i++ {
-					if data.Endpoints[i].Grade != detailsDomain[i].Grade ||
-						data.Endpoints[i].ServerName != detailsDomain[i].ServerName ||
-						data.Endpoints[i].IpAddress != detailsDomain[i].IpAddress{
-						changeServer = true
-					}
-				}
-			}else if len(data.Endpoints) > len(detailsDomain){
-				changeServer = true
-			}
-		}
-
-
+		changeServer = validateChangeServer(loc, payload, data, detailsDomain, changeServer)
 
 		if changeServer {
-			SaveDetailDomain(data, payload.ID, rp, w, r)
-		}*/
+			saveDetailDomain(data, payload.ID, rp, w, r)
+		}
+	}
+
+	currentGrade := getLowestGradeCurrent(data.Endpoints)
+	var previousGrade string
+
+	if detailsDomain == nil {
+		previousGrade = currentGrade
+	} else {
+		previousGrade = getLowestGradePrevious(detailsDomain)
 	}
 
 	//TODO Build return data
@@ -119,8 +91,8 @@ func (rp *Domain) GetByAddress(w http.ResponseWriter, r *http.Request) {
 
 	dataServer.Serves = servers
 	dataServer.ServersChanged = changeServer
-	dataServer.SslGrade = "A+"
-	dataServer.PreviousSslGrade = "B"
+	dataServer.SslGrade = currentGrade
+	dataServer.PreviousSslGrade = previousGrade
 	dataServer.Logo = ""
 	dataServer.Title = ""
 	dataServer.IsDown = false
@@ -132,16 +104,18 @@ func (rp *Domain) GetByAddress(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, dataServer)
 }
 
-func ValidateURL(address string) string {
-	space := regexp.MustCompile(`^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)`)
-	address = space.ReplaceAllString(address, "$1\u200B")
-	//address = strings.TrimSpace(address)
-	//address = strings.TrimLeft(address, "")
-	//address = strings.TrimRight(address, "")
-	return address
+func (rp *Domain) GetAllAddress(w http.ResponseWriter, r *http.Request) {
+	payload, err := rp.repo.GetAllDomain(r.Context())
+
+	if err != nil {
+		respondWithError(w, http.StatusNoContent, "Address not found")
+	}
+
+	respondWithJSON(w, http.StatusOK, payload)
 }
 
-func SaveDetailDomain(data modelSsl.SSL, idDomain int64, rp *Domain, w http.ResponseWriter, r *http.Request) {
+// Method that saves the main details of the domain or server
+func saveDetailDomain(data modelSsl.SSL, idDomain int64, rp *Domain, w http.ResponseWriter, r *http.Request) {
 	loc, _ := time.LoadLocation("America/Bogota")
 
 	for _, element := range data.Endpoints {
@@ -162,16 +136,6 @@ func SaveDetailDomain(data modelSsl.SSL, idDomain int64, rp *Domain, w http.Resp
 	}
 }
 
-func (rp *Domain) GetAllAddress(w http.ResponseWriter, r *http.Request) {
-	payload, err := rp.repo.GetAllDomain(r.Context())
-
-	if err != nil {
-		respondWithError(w, http.StatusNoContent, "Address not found")
-	}
-
-	respondWithJSON(w, http.StatusOK, payload)
-}
-
 // respondWithJSON write json response format
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
@@ -184,4 +148,89 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 // respondWithError return error message
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	respondWithJSON(w, code, map[string]string{"message": msg})
+}
+
+// We get the lowest grade from current servers
+func getLowestGradeCurrent(data []modelSsl.Endpoint) string {
+	var gradeAscii []int
+	var grade string
+
+	for _, dataElement := range data {
+		if dataElement.Grade != "A+" {
+			gradeAscii = append(gradeAscii, int(dataElement.Grade[0]))
+		} else {
+			grade = "A+"
+		}
+	}
+
+	sort.Slice(gradeAscii, func(i, j int) bool {
+		return gradeAscii[i] > gradeAscii[j]
+	})
+
+	grade = string(gradeAscii[0])
+	return grade
+}
+
+// We get the lowest grade from previous servers
+func getLowestGradePrevious(detail []modelDomain.DetailDomain) string {
+	var gradeAscii []int
+	var grade string
+
+	for _, dataElement := range detail {
+		if dataElement.Grade != "A+" {
+			gradeAscii = append(gradeAscii, int(dataElement.Grade[0]))
+		} else {
+			grade = "A+"
+		}
+	}
+
+	sort.Slice(gradeAscii, func(i, j int) bool {
+		return gradeAscii[i] > gradeAscii[j]
+	})
+
+	grade = string(gradeAscii[0])
+	return grade
+}
+
+// Validate if there is a change in the main data of the servers
+func validateChangeServer(loc *time.Location, payload modelDomain.Domain, data modelSsl.SSL, detailsDomain []modelDomain.DetailDomain, changeServer bool) bool {
+	hours := diffHours(loc, payload)
+	if hours >= 1 {
+		if len(data.Endpoints) == len(detailsDomain) {
+			for i := 0; i < len(data.Endpoints); i++ {
+				if data.Endpoints[i].Grade != detailsDomain[i].Grade ||
+					data.Endpoints[i].ServerName != detailsDomain[i].ServerName ||
+					data.Endpoints[i].IpAddress != detailsDomain[i].IpAddress {
+					changeServer = true
+				}
+			}
+		} else if len(data.Endpoints) > len(detailsDomain) {
+			changeServer = true
+		}
+	}
+	return changeServer
+}
+
+// Total hours difference
+func diffHours(loc *time.Location, payload modelDomain.Domain) float64 {
+	t1 := time.Date(time.Now().Year(),
+		time.Now().Month(),
+		time.Now().Day(),
+		time.Now().Hour(),
+		0, 0, 0, loc)
+
+	t2 := time.Date(payload.LastConsultation.Year(),
+		payload.LastConsultation.Month(),
+		payload.LastConsultation.Day(),
+		payload.LastConsultation.Hour(),
+		0, 0, 0, loc)
+
+	return t1.Sub(t2).Hours()
+}
+
+// Validate that the URL is cleaned
+func validateURL(address string) string {
+	space := regexp.MustCompile(`^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)`)
+	address = space.ReplaceAllString(address, "")
+	return address
 }
